873864857a6625ede8c8f277a94844e0b41bfdac
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-04 12:17:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import './App.css';
 
 class App extends Component {
     render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
+        return (
+
+            <ol key={"relativeList"}>
+                <li key={'relativeListItem1'}>Ramesh</li>
+                < li key={'relativeListItem2'} >Dhami</li>
+                < li key={'relativeListItem3'} >Kalki</li>
+            </ol>
+
         )
     }
 }
