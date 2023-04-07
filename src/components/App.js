8e51f3096f49d5f3c7c2239019f0ca926ce57cd4
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-04-07 17:04:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App = () => {
+    const fam = [
+        { id: "relativeListItem2", name: "Lee" },
+        { id: "relativeListItem1", name: "Jack" },
+        { id: "relativeListItem3", name: "Sparrow" },
+    ];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
-}
-
-
+    return (
+        <div id="main">
+            <ol id="relativeList">
+                <li id="relativeListItem1">Ben</li>
+                <li id="relativeListItem2">John</li>
+            </ol>
+        </div>
+    );
+};
 export default App;
